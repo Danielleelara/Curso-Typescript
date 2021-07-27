31f30c537a98494d4f18bc382f077e61f3f4fd07
Danielle Souza <danielle.andrade@helpper.com.br>
2021-07-27 18:53:49 -0300

    Adicionando arquivos

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const input1 = document.getElementById('num1');
-const input1 = document.getElementById('num1');
-const button = document.getElementById('button');
-
+"use strict";
+var input1 = document.getElementById('num1');
+var input2 = document.getElementById('num2');
+var button = document.getElementById('button');
 function sum(a, b) {
     return a + b;
 }
-
-button.addEventListener("click", function(){
-    console.log(sum(input1.nodeValue, input2.value));
+button.addEventListener('click', function () {
+    console.log(Number(input1.value), Number(input2.value));
 });
