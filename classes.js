98b248e6e4aa947de528bbe5c5d0bd303240811c
Danielle Souza <danielle.andrade@helpper.com.br>
2021-07-28 13:39:30 -0300

    Aula 09

---
+++ classes.js
@@ -0,0 +1,10 @@
+"use strict";
+var UserAccount = /** @class */ (function () {
+    function UserAccount(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    return UserAccount;
+}());
+var aluna = new UserAccount("Danielle", 36);
+console.log(aluna);
