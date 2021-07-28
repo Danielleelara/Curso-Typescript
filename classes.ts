0024146b1af606df16c1fca80b9a7a8eefa9e51c
Danielle Souza <danielle.andrade@helpper.com.br>
2021-07-28 13:39:58 -0300

    Aula 09 - complemento

---
+++ classes.ts
@@ -0,0 +1,12 @@
+class UserAccount {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number){
+        this.name = name;
+        this.age = age;
+    }
+}  
+
+const aluna = new UserAccount("Danielle", 36);
+console.log(aluna);
